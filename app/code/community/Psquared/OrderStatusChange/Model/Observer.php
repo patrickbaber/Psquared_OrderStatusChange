5d0e92f97123e7d23e6f1a1e8870294cbb931b80
Patrick Baber <patrick@p-squared.de>
2015-10-13 14:03:08 +0200

    add previous/next state/status to event array

--- app/code/community/Psquared/OrderStatusChange/Model/Observer.php
+++ app/code/community/Psquared/OrderStatusChange/Model/Observer.php
@@ -2,11 +2,16 @@
 
 class Psquared_OrderStatusChange_Model_Observer
 {
-	const REGISTRY_KEY = 'psquared_order_status';
-
+	protected $orderState;
 	protected $orderStatus;
 
 	public function fetchOrderStatus($observer) {
+		// state
+		if (!$this->orderState) {
+			$this->orderState = $observer->getOrder()->getState();
+		}
+
+		// status
 		if (!$this->orderStatus) {
 			$this->orderStatus = $observer->getOrder()->getStatus();
 		}
@@ -14,22 +19,38 @@ class Psquared_OrderStatusChange_Model_Observer
 	}
 
 	public function checkOrderStatus($observer) {
-		Mage::log('checkOrderStatus: ' . $this->orderStatus);
+		// state
+		if($this->orderState) {
+			$previousOrderState = $this->orderState;
+		}
+
+		// status
 		if(!$this->orderStatus) {
 			return;
 		} else {
 			$previousOrderStatus = $this->orderStatus;
 		}
 
-		//status change
+		// status change
 		$order = $observer->getOrder();
+		$newOrderState = $order->getState();
 		$newOrderStatus = $order->getStatus();
-		if ($previousOrderStatus != $newOrderStatus) {
-			//dispatch specific event, e.g. sales_order_status_change_pending_to_complete
-			Mage::dispatchEvent('sales_order_status_change_' . $previousOrderStatus . '_to_' . $newOrderStatus, array('order' => $order));
 
-			//general order change event
-			Mage::dispatchEvent('sales_order_status_change', array('order' => $order));
+		if ($previousOrderStatus != $newOrderStatus) {
+			// event data with order and statuses
+			$eventData = array(
+				'order'                 => $order,
+				'previousOrderState'    => $previousOrderState,
+				'newOrderState'         => $newOrderState,
+				'previousOrderStatus'   => $previousOrderStatus,
+				'newOrderStatus'        => $newOrderStatus,
+			);
+
+			// specific event, e.g. sales_order_status_change_pending_to_complete
+			Mage::dispatchEvent('sales_order_status_change_' . $previousOrderStatus . '_to_' . $newOrderStatus, $eventData);
+
+			// general order change event
+			Mage::dispatchEvent('sales_order_status_change', $eventData);
 		}
 	}
 }
